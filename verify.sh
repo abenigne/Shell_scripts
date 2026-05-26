8539ec2e9313636bd3717e0c897fd95764010ae0
Benigne Ingabire Akayesu <a.benigne@alustudent.com>
2026-05-26 10:42:59 +0200

    Adding a file

---
+++ verify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+Directory_Name="assignment_dir"
+Path="/C/Users/benig/Shell_scripts"
+if [ -d "$Path" ];
+then
+echo "Directory with a Name '$Directory_Name' exits in the Location"
+else
+echo "No such Directory"
+fi
+
+
